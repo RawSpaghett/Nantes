1a78b5d0e7b826654d3feec23f07333100ca2205
IRllyDntKnow <neriezra59@gmail.com>
2026-05-06 13:50:52 -0400

    Initialize player state machine architecture

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,14 +6,17 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] Transform playerCamera = null;
     [SerializeField] float mouseSensitivity = 5.0f;
+    [SerializeField] float moveSpeed = 5.0f;
 
     [SerializeField] bool lockCursor = true;
+    CharacterController controller = null;
 
     float cameraPitch = 0.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        controller = GetComponent<CharacterController>();
         if (lockCursor)
         {
             Cursor.lockState = CursorLockMode.Locked;
@@ -25,6 +28,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         UpdateMouseLook();
+        UpdateMovement();
     }
 
     void UpdateMouseLook()
@@ -33,11 +37,19 @@ public class PlayerController : MonoBehaviour
         Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 
         cameraPitch -= mouseDelta.y * mouseSensitivity;
-
         cameraPitch = Mathf.Clamp(cameraPitch, -90.0f, 90.0f);
 
         playerCamera.localEulerAngles = Vector3.right * cameraPitch;
-
         transform.Rotate(Vector3.up * mouseDelta.x * mouseSensitivity);
+    }
+    
+    void UpdateMovement()
+    {
+        Vector2 inputDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        inputDir.Normalize();
+
+        Vector3 velocity = (transform.forward * inputDir.y + transform.right * inputDir.x) * moveSpeed;
+    
+        controller.Move(velocity * Time.deltaTime);
     }
 }
