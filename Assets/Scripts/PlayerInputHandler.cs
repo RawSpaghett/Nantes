0654a61adf43dfaa15cf6235eccacaa36f120ef3
drewp03 <drewprato03@gmail.com>
2026-09-18 23:05:03 -0400

    added player movement and camera movement

---
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    [SerializeField] private PlayerMovement playerMovement;
+    public Vector2 move, look;
+    public bool sprintHeld;
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+    }
+
+    public void OnLookInput(InputAction.CallbackContext context)
+    {
+        look = context.ReadValue<Vector2>();
+    }
+
+    public void OnJumpInput(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+            playerMovement.Jump();
+        }
+    }
+
+    public void OnSprintInput(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+            sprintHeld = true;
+        }
+
+        if(context.canceled)
+        {
+            sprintHeld = false;
+        }
+    }
+
+}
